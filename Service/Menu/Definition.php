1598dd383f2ccbcc64a9fb2192c0bc20977cf2bb
Laurent Minguet <laurent@spareka.com>
2019-07-16 15:17:36 +0200

    fix - add default menu definition

---
+++ Service/Menu/Definition.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of a Spipu Bundle
+ *
+ * (c) Laurent Minguet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Spipu\UiBundle\Service\Menu;
+
+use Spipu\UiBundle\Entity\Menu\Item;
+
+/**
+ * Class Definition - Example. Make your own menu definition !
+ * @package Spipu\UiBundle\Service\Menu
+ */
+class Definition implements DefinitionInterface
+{
+    /**
+     * @var Item
+     */
+    private $mainItem;
+
+    /**
+     * @return void
+     */
+    private function build(): void
+    {
+        $this->mainItem = new Item('Main');
+    }
+
+    /**
+     * @return Item
+     */
+    public function getDefinition(): Item
+    {
+        if (!$this->mainItem) {
+            $this->build();
+        }
+
+        return $this->mainItem;
+    }
+}
