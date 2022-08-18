57c9ce9c628a41f95808354893c8bc0048568b02
Laurent Minguet <laurent@spareka.com>
2022-08-18 12:51:34 +0200

    better std code

--- Entity/GridConfig.php
+++ Entity/GridConfig.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This file is part of a Spipu Bundle
+ *
+ * (c) Laurent Minguet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Spipu\UiBundle\Entity;
 
 use Spipu\UiBundle\Repository\GridConfigRepository;
