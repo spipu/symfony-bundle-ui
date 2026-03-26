5549d21c748cf982ca51bf4228fc4d3fd5b0a581
Victor Shvets <12849028+f0ska@users.noreply.github.com>
2026-03-26 17:26:20 +0200

    visual improvement for acl form

--- public/js/spipu-checkbox-tree.js
+++ public/js/spipu-checkbox-tree.js
@@ -10,21 +10,47 @@ class SpipuUiCheckboxTree {
     init () {
         let that = this;
 
-        let inputs = this.mainNode.find('input[type=checkbox]');
+        let inputs = this.mainNode.find('input[type=checkbox],input[type=radio]');
 
         inputs.on('change', function () { that.change($(this)); });
         inputs.each(function() { that.toggleChildren($(this)); });
     }
 
     change(node) {
-        node.closest('li').find('ul li input[type=checkbox]').prop('checked', false);
-
         this.toggleChildren(node);
-
     }
 
     toggleChildren(node) {
-        node.closest('li').find('ul').toggleClass('useless-node', node.prop('checked'));
+        let selector = this.getFlatChildrenSelector(node);
+        let form = node.closest('form');
+        this.toogleUselessNode(form.find('input'), false);
+        this.toogleUselessNode(form.find(selector), true)
+        this.toogleUselessNode(form.find('input:checked:not(:indeterminate) ~ ul > li > input'), true);
+    }
+
+    getFlatChildrenSelector(node) {
+        let selectors = [], list = [];
+        node.closest('form').find('input[data-children]:checked').each(function (index, checkbox) {
+            let flat = $(checkbox).attr('data-children');
+            if (!flat || flat.length === 0) {
+                return;
+            }
+            list = [...list, ...flat.split(',')];
+        });
+        for (let i in list) {
+            selectors.push('input[value="' + list[i] + '"]');
+        }
+        return selectors.join(',');
+    }
+
+    toogleUselessNode(node, checked) {
+        node.prop('indeterminate', checked)
+            .prop('disabled', checked)
+            .each(function (index, checkbox) {
+                $(checkbox).closest('li').find('ul input')
+                    .prop('disabled', checked)
+                    .prop('indeterminate', checked);
+            })
     }
 }
 
