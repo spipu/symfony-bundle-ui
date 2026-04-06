d325632d78eba97063220d4d267d878c33eac762
spipu <spipu45@gmail.com>
2026-04-06 23:19:35 +0200

    Improve code quality

--- src/Command/ResetGridConfigCommand.php
+++ src/Command/ResetGridConfigCommand.php
@@ -14,10 +14,12 @@ declare(strict_types=1);
 namespace Spipu\UiBundle\Command;
 
 use Spipu\UiBundle\Repository\GridConfigRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(name: 'spipu:ui:grid-config:reset', description: 'Reset the Spipu UI Grid Config.')]
 class ResetGridConfigCommand extends Command
 {
     private GridConfigRepository $gridConfigRepository;
@@ -33,8 +35,6 @@ class ResetGridConfigCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setName('spipu:ui:grid-config:reset')
-            ->setDescription('Reset the Spipu UI Grid Config.')
             ->setHelp('This command allows you to reset the Spipu UI Grid Config')
         ;
     }
