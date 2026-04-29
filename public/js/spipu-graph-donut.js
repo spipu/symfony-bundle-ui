1f4dd4dc64991a7564addf1cf63b9f52cba9f640
Laurent Minguet <laurent@spareka.com>
2026-04-29 11:41:22 +0200

    Prepare new widget donut type

--- public/js/spipu-graph-donut.js
+++ public/js/spipu-graph-donut.js
@@ -8,9 +8,31 @@ class SpipuGraphDonut {
     ) {
         this.graph = new GoogleGraphDonut(
             destinationId,
-            data,
+            SpipuGraphDonut.toGoogleData(data),
             options
         );
+    }
+
+    static toGoogleData(data) {
+        let total = 0;
+        for (let i = 0; i < data.length; i++) {
+            total += Number(data[i].value) || 0;
+        }
+        if (total <= 0) {
+            total = 1;
+        }
+
+        let rows = [['Label', 'Value']];
+        for (let i = 0; i < data.length; i++) {
+            let value = Number(data[i].value) || 0;
+            let percent = (100 * value / total).toFixed(1);
+            rows.push([
+                data[i].label + ' (' + value + ' - ' + percent + '%)',
+                value
+            ]);
+        }
+
+        return rows;
     }
 }
 
